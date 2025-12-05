e2c62a1b34af7d7fd4ce40365280a0ba76b4c7a0
ryaalbr <ryaalbr@nuevaschool.org>
2025-12-05 09:12:40 -0800

    edit

--- public/config.js
+++ public/config.js
@@ -1,6 +1,1 @@
-// IMPORTANT: Replace YOUR_GOOGLE_CLIENT_ID_HERE with your actual Google Client ID
-// Get your Client ID from: https://console.cloud.google.com/
-// This file should NOT be committed to version control (it's in .gitignore)
-window.APP_CONFIG = {
-  GOOGLE_CLIENT_ID: '934910355431-m9rlcsrdfdfh6tfjj3usruah4oabm5bj.apps.googleusercontent.com'
-}; 
+
